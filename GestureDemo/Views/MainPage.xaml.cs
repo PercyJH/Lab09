5621584d14fd093c82c3533a421bc8f40bc07bcf
Your Name <you@example.com>
2021-10-16 18:17:22 -0500

    done

---
+++ GestureDemo/Views/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GestureDemo.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnTap.Clicked += async(sender, e) => {
+                await Navigation.PushAsync(new TapDemo());
+            };
+            btnPinch.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PinchDemo());
+            };
+            btnPan.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PanDemo());
+            };
+            btnSwipe.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SwipeDemo());
+            };
+        }
+    }
+}
---
+++ GestureDemo/Views/TapDemo.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GestureDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TapDemo : ContentPage
+    {
+        int tapCount;
+        readonly Label label;
+        public TapDemo()
+        {
+            InitializeComponent();
+            var image = new Image
+            {
+                Source = "tapped.png",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.NumberOfTapsRequired = 2;
+            tapGestureRecognizer.Tapped += OnTapGestureRecognizerTapped;
+            image.GestureRecognizers.Add(tapGestureRecognizer);
+
+            label = new Label
+            {
+                Text = "Tap the photo!",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(20, 100),
+                Children =
+                {
+                    image,
+                    label
+                }
+            };
+
+            void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+            {
+                tapCount++;
+                label.Text = String.Format("{0} Tap{1} so Far!",
+                    tapCount,
+                    tapCount == 1 ? "" : "s");
+
+                var imageSender = (Image)sender;
+
+                if(tapCount % 2 == 0)
+                {
+                    imageSender.Source = "tapped.png";
+                }
+                else
+                {
+                    imageSender.Source = "tapped_bw.png";
+                }
+            }
+        }
+    }
+}
